f88b97ddd706da43fa886cf487540de0df60a470
Dogbone0714 <jameskang0714@gmail.com>
2020-10-10 06:31:13 +0800

    add

--- A36.cpp
+++ A36.cpp
@@ -4,9 +4,16 @@
 using namespace std;
 
 int main(void){
-
-
-
-
+	int a,b=0;
+ 	cin>>a;
+ 	for(int i=1;i<=a;i++){
+ 		for(int j=1;j<=i;j++){
+ 		b++;
+ 		cout<<b%10;
+ 		}
+ 	cout<<endl;
+ 	}
+ 	return 0;
 }
+
 
