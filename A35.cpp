3c9aa1471d495090c2b1dd14e4190167b159b607
Dogbone0714 <jameskang0714@gmail.com>
2020-10-10 06:17:43 +0800

    Create A35.cpp

---
+++ A35.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main(void){
+	int a;
+ 	cin>>a;
+ 	for(int i=2;i<=a*2;i+=2){
+ 		for(int j=i;j<i+i;j+=2){
+ 		cout<<j<<" ";
+ 		}
+ 	cout<<endl;
+ 	}
+ return 0;
+
+
+
+}
+
