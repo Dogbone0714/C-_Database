15be747ce682258160182af4276dd4a6d0cef57d
Dogbone0714 <jameskang0714@gmail.com>
2020-10-10 06:21:19 +0800

    Update A35.cpp

--- A35.cpp
+++ A35.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 using namespace std;
-
-int main(void){
-	int a;
- 	cin>>a;
- 	for(int i=2;i<=a*2;i+=2){
- 		for(int j=i;j<i+i;j+=2){
- 		cout<<j<<" ";
- 		}
- 	cout<<endl;
- 	}
+int main(int argc, char** argv) {
+ int a,b;
+ cin>>a;
+ b=sqrt(a);
+ if(a==1){
+ cout<<"NOT PRIME"<<endl;
+ }
+ if(a==2||a==3){
+ cout<<"PRIME"<<endl; 
+ }
+ for(int i=2;i<=b;i++){
+ if(a%i==0){
+ cout<<"NOT PRIME"<<endl;
+ break;
+ }
+ if(i==b){
+ cout<<"PRIME"<<endl;
+ }
+ }
  return 0;
-
-
-
 }
-
